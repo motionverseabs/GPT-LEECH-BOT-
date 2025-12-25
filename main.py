75f9b84de05f48e485bc00aeb5f4c6e858d87c26
motionverseabs <absmotionverse@gmail.com>
2025-12-25 21:02:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+@Client.on_message(filters.command("cancel"))
+async def cancel_handler(client: Client, message: Message):
+    await message.reply_text("❌ Cancel not supported in v1 (coming soon).")
 from pyrogram import Client
 from bot.config import BOT_TOKEN, API_ID, API_HASH
 from bot.utils.logger import get_logger
