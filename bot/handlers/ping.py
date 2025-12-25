aef37acebda0fd6914613e3f22c6de7d57b5e0b0
motionverseabs <absmotionverse@gmail.com>
2025-12-25 20:52:41 +0530

    Create ping.py

---
+++ bot/handlers/ping.py
@@ -0,0 +1,6 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+@Client.on_message(filters.command("ping"))
+async def ping_handler(client: Client, message: Message):
+    await message.reply_text("🏓 Pong! Bot is alive.")
